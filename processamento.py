49200c54082400b252b91c198a75373a301ebf6c
RafaExMachina <fael1928@gmail.com>
2026-08-02 13:50:03 -0300

    coreçoes

--- processamento.py
+++ processamento.py
@@ -1,9 +1,3 @@
-import pandas as pd
-import os #Import não utilizado
-import sys
-
 def limpa_dados(df):
-    #espaçamento errado, variaveis não utilizadas
-    x=10
     df_limpo = df.dropna() #Remove linhas com valores nulos
     return df_limpo
